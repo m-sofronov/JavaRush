0e9707b321f93d50bc146579f0d48782bafbf5b7
real-primarchvg <primarchvg@gmail.com>
2018-04-03 17:20:43 +0300

    Second

--- src/com/javarush/task/task07/task0712/Solution.java
+++ src/com/javarush/task/task07/task0712/Solution.java
@@ -16,14 +16,27 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         ArrayList<String> arrayList = new ArrayList<String>();
-        int max = 0, min = 9999999;
+        int max = 0, min = 0;
 
         for (int i = 0; i < 10; i++) {
             arrayList.add(reader.readLine());
             //int temp = arrayList.get(i).length();
-            if (arrayList.get(i).length() > max) max = arrayList.get(i).length();
-            if (arrayList.get(i).length() < min) min = arrayList.get(i).length();
+//            if (arrayList.get(i).length() > max) max = arrayList.get(i).length();
+//            if (arrayList.get(i).length() < min) min = arrayList.get(i).length();
         }
+
+        for (int i = 1; i < arrayList.size(); i++) {
+            if (arrayList.get(max).length() < arrayList.get(i).length()) {
+                max = i;
+            }
+            if (arrayList.get(min).length() > arrayList.get(i).length()) {
+                min = i;
+            }
+        }
+        if (max < min) {
+            System.out.println(arrayList.get(max));
+        } else
+            System.out.println(arrayList.get(min));
 
 //        System.out.println(max + " " + min);
 
