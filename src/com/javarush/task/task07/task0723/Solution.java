62f02a1dbf4467747b64d6ccb969dee12d3b2eae
real-primarchvg <primarchvg@gmail.com>
2018-04-23 18:25:57 +0300

    Add 'Обратынй отсчёт'

---
+++ src/com/javarush/task/task07/task0723/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task07.task0723;
+
+/* 
+Обратный отсчёт
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 30; i >= 0; i--) {
+
+            System.out.println(i);
+
+            //напишите тут ваш код
+
+            try
+            {
+                Thread.currentThread().sleep(100);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Бум!");
+    }
+}
---
+++ src/com/javarush/task/task07/task0724/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task07.task0724;
+
+/* 
+Семейная перепись
+*/
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        ArrayList<Human> family = new ArrayList<Human>();
+    }
+
+    public static class Human {
+        //напишите тут ваш код
+
+        public String toString() {
+            String text = "";
+            text += "Имя: " + this.name;
+            text += ", пол: " + (this.sex ? "мужской" : "женский");
+            text += ", возраст: " + this.age;
+
+            if (this.father != null)
+                text += ", отец: " + this.father.name;
+
+            if (this.mother != null)
+                text += ", мать: " + this.mother.name;
+
+            return text;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
