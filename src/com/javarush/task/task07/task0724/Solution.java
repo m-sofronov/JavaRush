d8ca255378c55f10638da6871154362f889e0fb2
real-primarchvg <primarchvg@gmail.com>
2018-04-28 17:41:56 +0300

    Add 'Семейная перепись'

--- src/com/javarush/task/task07/task0724/Solution.java
+++ src/com/javarush/task/task07/task0724/Solution.java
@@ -9,11 +9,49 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        ArrayList<Human> family = new ArrayList<Human>();
+        Human grandfather1 = new Human("gDad1", true, 60);
+        Human grandfather2 = new Human("gDad2", true, 54);
+        Human grandmother1 = new Human("gMom1", false, 57);
+        Human grandmother2 = new Human("gMom2", false, 54);
+
+        Human father = new Human("Dad", true, 35, grandfather1, grandmother1);
+        Human mother = new Human("Mom", false, 27, grandfather2, grandmother2);
+
+        Human child1 = new Human("Child1", true, 7, father, mother);
+        Human child2 = new Human("Child2", false, 6, father, mother);
+        Human child3 = new Human("Child3", true, 1, father, mother);
+
+        System.out.println(grandfather1);
+        System.out.println(grandfather2);
+        System.out.println(grandmother1);
+        System.out.println(grandmother2);
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(child1);
+        System.out.println(child2);
+        System.out.println(child3);
     }
 
     public static class Human {
         //напишите тут ваш код
+        String name;
+        boolean sex;
+        int age;
+        Human father;
+        Human mother;
+
+        public Human(String name, boolean sex, int age){
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        };
+        public Human(String name, boolean sex, int age, Human father, Human mother){
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.father = father;
+            this.mother = mother;
+        };
 
         public String toString() {
             String text = "";
